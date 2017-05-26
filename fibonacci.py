1cba5d1a03132c437c72feed4fcc61ae12a69c61
Donald Flynn <donaldflynn@DN525jj8.SUNet>
2017-05-25 17:56:40 -0700

    I have completed both of the functions recursively

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,26 @@
 
 # Lab 16: recursion
 # Fibonacci with cache
+
+import sys
+sys.setrecursionlimit(5000000)
+
+global fib_cache
+fib_cache = {}  # Dictionary
+
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n in fib_cache:
+        return fib_cache.get(n)
+    elif n == 1:
+        return 1
+    else:
+        x = fib(n-1) + fib(n-2)
+        fib_cache[n] = x
+        return x
+
+
+
--- levenshtein.py
+++ levenshtein.py
@@ -2,3 +2,25 @@
 
 # Lab 16: Recursion
 # Levenshtein Distance
+
+
+def lev(s1, s2):
+    if s1 == "" and s2 == "":
+        return 0
+    elif s1 == "":
+        return (1 + lev(s1, s2[1:]))
+    elif s2 == "":
+        return (1 + lev(s1[1:], s2))
+    elif s1[0] == s2[0]:
+        return lev(s1[1:],s2[1:])
+    else:
+        x = 1 + lev(s1[1:], s2)
+        y = 1 + lev(s1, s2[1:])
+        z = 1 + lev(s1[1:], s2[1:])
+        return min(x,y,z)
+    
+        
+    
+    
+    
+    
